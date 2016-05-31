976326ca2551d7f5b9c18b995e984411188c2d2b
OscarHiNuWar <oscarhinuwar@gmail.com>
2016-05-31 10:53:32 -0400

    Copia de seguridad Mafri
    Creado el 31/05/16

---
+++ mafri/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link href="css/template.css" rel="stylesheet"/>
+ <script src="js/jquery-1.12.2.min.js"></script>
+    <script src="js/parallax.js"></script>
+
+<title>MafriElectric - Equipos Eléctricos e Industriales</title>
+</head>
+
+<body>
+<header>
+	<div id="content-dir-top">
+    	<div id="dir-top">Materiales Eléctricos e Industriales, Llámanos: 809-583-4255</div>
+    </div>
+    <div id="content-menu">
+    	<div id="center-menu">
+        	<nav>
+        		<div id="logo"></div>
+            	<div id="nav-menu">
+                	<ul>
+                    	<a href="#"><li>Productos</li></a>
+                        <a href="#"><li>Proyectos</li></a>
+                        <a href="#"><li>Descargas</li></a>
+                        <a href="#"><li>La empresa</li></a>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
+
+    <div id="banner"  class="parallax-window" data-parallax="scroll" data-image-src="images/central.jpg">
+    	<div id="over-azul"></div>
+    </div>
+    <div id="content-fo-te">
+    	<div id="content-fo">
+        	<div class="foto-gris-1"><img src="images/01.jpg"></div>
+            <div class="foto-gris-1"><img src="images/02.jpg"></div>
+            <div class="foto-gris-1" style="margin-right:0;"> <img src="images/03.jpg"> </div>
+            <div class="foto-gris-2">  <img src="images/04.jpg"> </div>
+            <div class="foto-gris-2"> <img src="images/05.jpg"> </div>
+            <div class="foto-gris-2"> <img src="images/06.jpg"> </div>
+            <div class="foto-gris-2"> <img src="images/07.jpg"> </div>
+            
+        </div>
+        <div id="content-te">
+			<div class="informacion-uno">
+				<div class="contenedor-interno">
+					<h1>Qué es Mafri Electric?</h1>
+					<p>nivel nacional e internacional. Nuestra organización 
+					siempre busca de forma profesional, superar las 
+					expectativas... <a href="#">Leer Más</a></p>
+				</div>
+			</div>
+			<div class="informacion-uno">
+				<div class="contenedor-interno">
+						<h1 class="h1-home">Es una empresa dedicada a proveer productos de iluminación y control de la más alta calidad</h1>
+						<p>nivel nacional e internacional. Nuestra organización 
+						siempre busca de forma profesional, superar las 
+						expectativas... <a href="#">Leer Más</a></p>
+					</div>
+			</div>
+		</div>
+    </div>
+    
+    <div id="content-bol-des">
+    	<div id="content-bol">
+			<div class="informacion-dos">
+					<div class="alineo">
+						<div class="fonts"><p>Es una empresa dedicada a proveer productos 
+						de iluminación y control de la más alta calidad</p></div>
+						<p>nivel nacional e internacional. Nuestra organización 
+							siempre busca de forma profesional, superar las 
+							expectativas... <a href="#">Leer Más</a></p>
+					</div>
+			</div>
+			
+			<div class="informacion-dos">
+					<div class="alineo">
+						<div class="fonts"><p>es una empresa dedicada a proveer productos
+						de iluminación y control de la más alta calidad</p></div>
+						<p>nivel nacional e internacional. Nuestra organización 
+							siempre busca de forma profesional, superar las 
+							expectativas... <a href="#">Leer Más</a></p>
+					</div>
+			</div>
+		</div>
+        <div id="content-des">
+			<div class="des-titulo">Boletín Informativo</div>
+            <div class="des-texto">Reciba nuestras ofertas en su correo</div>
+            <div class="content-form-des">
+            	<form action="" method="post">
+                	<div class="input-des"><input name="" type="text" class="input" placeholder="Su e-mail" /></div>
+                    <div class="boton-des">Enviar</div>
+                </form>
+            </div>
+            <div class="span-text">*No enviamos span</div>
+		</div>
+    </div>
+    <div id="logos-content"></div>
+    <footer>
+    	<div id="footer">
+        	<div id="center-footer">
+            	<div id="text-fo-1">© Copyright 2015. Todos los derechos reservados. Mafri Electric</div>
+                <div id="text-fo-2"><span>Diseño y desarrollo </span>Agencia 7am</div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
